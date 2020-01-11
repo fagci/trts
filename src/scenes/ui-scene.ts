53453acc0390544e9dfc2a0bb9382924772a98f6
fagcinsk <fagci.nsk@gmail.com>
2020-01-12 03:58:27 +0700

    ui-scene

--- src/scenes/ui-scene.ts
+++ src/scenes/ui-scene.ts
@@ -4,15 +4,25 @@ export default class UIScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(0, 0, 'swss', 'plane')
-    this.debugText = this.add.text(10, 10, `Hello`, { font: '18px monospace', fill: '#000' })
-    this.debugText.setScrollFactor(0)
+    const paneRect = new Phaser.Geom.Rectangle(0,0, 240, 128)
+    this.add.graphics()
+      .fillStyle(0x224466, 0.55)
+      .lineStyle(1, 0x224466,0.75)
+      .fillRectShape(paneRect)
+      .strokeRectShape(paneRect)
+    this.debugText = this.add.text(10, 10, `Hello`, { font: '14px monospace', fill: '#fff' })
   }
 
   update() {
     const mainCamera = this.scene.get('MainScene').cameras.main
-    this.debugText.setText(`Camera pos:
-X: ${mainCamera.centerX} Y: ${mainCamera.centerY}
+    
+    const CX = mainCamera.scrollX + mainCamera.centerX
+    const CY = mainCamera.scrollY + mainCamera.centerY
+
+    const CTX = CX >> 4
+    const CTY = CY >> 4
+
+    this.debugText.setText(`Camera pos: X ${CTX.toFixed(0)}, Y ${CTY.toFixed(0)}
 Camera zoom: ${mainCamera.zoom.toFixed(2)}`)
   }
 }
